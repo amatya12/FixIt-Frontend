e2a55ba998bb42e1f475585efcba505de0a92909
madasteroid <prakash.timalsina@selu.edu>
2020-02-12 00:28:11 -0600

    aspect ratio added to map

--- app/components/ViewMap.js
+++ app/components/ViewMap.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker, AnimatedRegion} from 'react-native-maps';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Dimensions,
+    TouchableOpacity,
+    AlertIOS,
+} from 'react-native';
 import styles from '../styles/ViewMap.styles';
+
+const screen = Dimensions.get('window');
+
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 
 export class ViewMap extends React.Component {
@@ -13,12 +27,13 @@ export class ViewMap extends React.Component {
                 initialRegion={{
                     latitude: 30.460929,
                     longitude: -89.987643,
-                    latitudeDelta: 0.51,
-                    longitudeDelta: 0.51,
+                    latitudeDelta: LATITUDE_DELTA,
+                    longitudeDelta: LONGITUDE_DELTA,
                 }}
                 showsUserLocation ={true}
+            >
 
-            />
+            </MapView>
         );
     }
 }
