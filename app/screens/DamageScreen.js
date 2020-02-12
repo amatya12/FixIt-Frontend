34b5fbb8f5021791429e12d57db326c630b50388
madasteroid <prakash.timalsina@selu.edu>
2020-02-12 00:00:05 -0600

    Damaged screen added

---
+++ app/screens/DamageScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ViewMap} from "../components/ViewMap";
+
+
+export default function DamageScreen() {
+
+  return (
+      <ViewMap/>
+  );
+
+}
+
+DamageScreen.navigationOptions = {
+  title: 'Damages',
+};
