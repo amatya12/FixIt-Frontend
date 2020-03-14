320f3a7681326847bd11c4d19c6c539105d8aab2
Aakash <aakashamatya63@gmail.com>
2020-03-14 16:57:07 -0500

    changes in ajaz.js

---
+++ app/ajax.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default {
+    fetchInitialDepartments() {
+        console.log('*************');
+
+        // axios.get('http://192.168.1.249:45455/api/category').then(res => console.log("The response is ", res))
+        //     .catch((error) => { http://192.168.1.249:5001/api/category
+        //         // handle error
+        //         console.log("Error is ", error);
+        //     })
+
+        axios.get('http://192.168.1.249:5001/api/category').then(res => console.log(res.data))
+            .catch((error) => {
+                // handle error
+                console.log("Error is ", error);
+            });
+
+
+
+    }
+};
