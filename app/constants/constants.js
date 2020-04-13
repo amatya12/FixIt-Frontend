ddf0221eaddbdbf70c25c9859bd855a1e1bd9057
Aakash <aakashamatya63@gmail.com>
2020-04-12 20:22:03 -0500

    production url

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,1 +1,3 @@
-export const API_ENDPOINT = "http://192.168.1.25:5001/api";
+//export const API_ENDPOINT = "http://192.168.1.249:5001/api";
+
+export const API_ENDPOINT = "http://api.fixit-backend.com/api";
